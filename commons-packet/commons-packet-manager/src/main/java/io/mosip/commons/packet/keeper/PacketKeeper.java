18bf9bdc30a569ebd3d8abdb8257dbde92f325f0
Nidhi0201 <nidhi.k@cyberpwn.com>
2025-08-13 19:18:15 +0530

    unnecessary log removed
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
@@ -162,8 +162,6 @@ public class PacketKeeper {
             }
             byte[] subPacket = getCryptoService().decrypt(helper.getRefId(
                     packet.getPacketInfo().getId(), packet.getPacketInfo().getRefId()), encryptedSubPacket);
-            boolean sub = subPacket == null;
-            LOGGER.info("subPacket:: "+ sub);
             packet.setPacket(subPacket);
 
 
